993d3e509121f9d21ef6dd4ce9b6984aedee2a17
Pedro Henrique <67080125+pedro-brandao@users.noreply.github.com>
2022-07-15 16:01:35 +0000

    correcting exercice

--- aula-python/exercicios/ex016.py
+++ aula-python/exercicios/ex016.py
@@ -3,3 +3,32 @@
 Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 '''
 # ex: digite um numero: 6.127 > o numero 6.127 tem 6 como porção inteira.
+
+from math import floor
+
+num = float(input('Digite um numero flutuante!:'))
+
+real = floor(num)
+
+print('O numero digitado arredondado é {}.'.format(real))
+
+
+# Prof. Guanabara
+'''
+######  metodo importando  ######
+from math import trunc
+
+num = float(input('Digite um numero flutuante!:'))
+
+real = trunc(num)
+
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, real))
+
+######  metodo sem importar  ######
+
+num_2 = float(input("Digite um numero!:"))
+
+print("O valor digitado foi {} e a sua parte inteira é {}".format(num, int(num)))
+
+'''
+
--- aula-python/exercicios/ex017.py
+++ aula-python/exercicios/ex017.py
@@ -1,3 +1,4 @@
 '''
 Faça um programa que leia o comprimento do cateto oposto e a do cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa.
 '''
+
