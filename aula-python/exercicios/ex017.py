3a97b760d17f48be83819766a88f822b362e3faf
Pedro Henrique <67080125+pedro-brandao@users.noreply.github.com>
2022-07-19 15:56:22 +0000

    new exercice add

--- aula-python/exercicios/ex017.py
+++ aula-python/exercicios/ex017.py
@@ -2,3 +2,21 @@
 Faça um programa que leia o comprimento do cateto oposto e a do cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa.
 '''
 
+# ------------- Codigo Prof. Guanabara -------------------
+from math import hypot
+
+co = float(input("Digite o valor do cateto oposto: "))
+ca = float(input("Digite o valor do cateto adjacente: "))
+
+hi = (co ** 2 + ca ** 2 ) ** (1/2)
+
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+# --------------------------------------------------------
+''' Usando Modulos '''
+
+co = float(input("Digite o valor do cateto oposto: "))
+ca = float(input("Digite o valor do cateto adjacente: "))
+
+hip = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hip))
