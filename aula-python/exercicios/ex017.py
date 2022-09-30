f7894ec498dced4d0dfd959d4fa097140be79013
Pedro Henrique <67080125+pedro-brandao@users.noreply.github.com>
2022-09-30 16:04:39 +0000

    new exercice.

--- aula-python/exercicios/ex017.py
+++ aula-python/exercicios/ex017.py
@@ -2,7 +2,7 @@
 Faça um programa que leia o comprimento do cateto oposto e a do cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa.
 '''
 
-# ------------- Codigo Prof. Guanabara -------------------
+# ------------- Codigo Prof. Guanabara sem modulos -------------------
 from math import hypot
 
 co = float(input("Digite o valor do cateto oposto: "))
@@ -12,7 +12,7 @@ hi = (co ** 2 + ca ** 2 ) ** (1/2)
 
 print('A hipotenusa vai medir {:.2f}'.format(hi))
 
-# --------------------------------------------------------
+# ------------- Codigo Prof. Guanabara com modulos -------------------
 ''' Usando Modulos '''
 
 co = float(input("Digite o valor do cateto oposto: "))
--- aula-python/exercicios/ex018.py
+++ aula-python/exercicios/ex018.py
@@ -1,3 +1,8 @@
 '''
 Faça um programa que leia um angulo qualquer e mostre na tela o valo do seno, cosseno e tangente desse angulo.
 '''
+
+angulo = float(input("Digite o angulo que você quer: "))
+seno = math.sin(math.radians(angulo))
+
+print("O angulo {} tem SENO de {}".format(angulo, seno))
